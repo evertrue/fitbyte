13b638151667510c4616947d4c0ca39303254bcf
Bob Breznak <bob.breznak@gmail.com>
2014-01-25 11:05:48 -0500

    Setup initial project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Fitbyte::Application.config.secret_key_base = 'bffe1306b105fd991bec068ce158d993d59c7ad7dd22e4362969d82be94144a6126a0cd26c69482ca6097476daba348eaa98c579727b0c3c47d10f8450f2f2ec'
