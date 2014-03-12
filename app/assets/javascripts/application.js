a8a405b21b520e824a489cc75827683db7e3a99d
Bob Breznak <bob.breznak@gmail.com>
2014-03-12 08:52:40 -0400

    Add fitbit login

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,3 +2,13 @@
 //= require jquery_ujs
 //= require twitter/bootstrap
 //= require_tree .
+
+Number.prototype.number_with_delimiter = function(delimiter) {
+    var number = this + '', delimiter = delimiter || ',';
+    var split = number.split('.');
+    split[0] = split[0].replace(
+        /(\d)(?=(\d\d\d)+(?!\d))/g,
+        '$1' + delimiter
+    );
+    return split.join('.');    
+};
